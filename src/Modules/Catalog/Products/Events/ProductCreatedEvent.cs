63775db2759cce0b052af83ff90d216a97e17dba
bravephoenix.learn <bravephoenix.learn@gmail.com>
2025-03-21 00:48:21 +0530

    Added CatalogDBContext

--- src/Modules/Catalog/Products/Events/ProductCreatedEvent.cs
+++ src/Modules/Catalog/Products/Events/ProductCreatedEvent.cs
@@ -1,12 +1,4 @@
-﻿using Catalog.Products.Models;
-using Shared.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Catalog.Products.Events
+﻿namespace Catalog.Products.Events
 {
     public record ProductCreatedEvent(Product product) : IDomainEvent;
 }
