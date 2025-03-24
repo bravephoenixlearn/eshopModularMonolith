7c66426227c2f9f5239f2b8b38fb60f60322f357
bravephoenix.learn <bravephoenix.learn@gmail.com>
2025-03-24 21:20:57 +0530

    added Product module features

---
+++ src/Modules/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Products.Features.GetProductsByCategory;
+using Catalog.Products.Features.UpdateProduct;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Catalog.Products.Features.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(ProductDto Product);
+
+    internal class GetProductByIdHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var product = await dbContext.Products.AsNoTracking().SingleOrDefaultAsync(p=>p.Id == query.Id, cancellationToken);
+
+            if (product is null)
+                throw new Exception($"Product not found: {query.Id}");
+
+            var productDto = product.Adapt<ProductDto>();
+            return new GetProductByIdResult(productDto);
+        }
+    }
+}
---
+++ src/Modules/Catalog/Products/Features/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.Products.Features.UpdateProduct
+{
+    public record UpdateProductComamnd(ProductDto Product)
+        : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+
+    internal class UpdateProductHandler(CatalogDbContext dbContext) : ICommandHandler<UpdateProductComamnd, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductComamnd command, CancellationToken cancellationToken)
+        {
+            var product = await dbContext.Products.FindAsync([command.Product.Id], cancellationToken);
+
+            if (product is null)
+                throw new Exception($"Product not found: {command.Product.Id}");
+
+            UpdateProductWithNewValues(product, command.Product);
+            dbContext.Products.Update(product);
+            await dbContext.SaveChangesAsync();
+            return new UpdateProductResult(true);
+        }
+
+        private void UpdateProductWithNewValues(Product product, ProductDto productDto)
+        {
+            product.Update(productDto.Name, productDto.Categories, productDto.Description, productDto.ImageFile, productDto.Price);
+        }
+    }
+}
