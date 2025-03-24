d364ef9842f6dda08c3fa8693d76b79da84adcc8
bravephoenix.learn <bravephoenix.learn@gmail.com>
2025-03-24 18:18:30 +0530

    added absatractions for shared CQRS

---
+++ src/Modules/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductCommand
+        (string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
