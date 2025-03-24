37bf2eaf03982f0c98a55782d7779b5fba42f622
bravephoenix.learn <bravephoenix.learn@gmail.com>
2025-03-24 21:22:15 +0530

    added Product module event handlers

---
+++ src/Modules/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Products.EventHandlers
+{
+    public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
+        : INotificationHandler<ProductPriceChangedEvent>
+    {
+        public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain event handled: {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
